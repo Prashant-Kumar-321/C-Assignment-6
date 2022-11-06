87515ae2991b33a028e43e8b1249642001a38af1
Prashant-Kumar-321 <pk6176838@gmail.com>
2022-11-06 12:49:47 +0530

    Assignment6

---
+++ 2_Sum_Even_N_natural_number.c
@@ -0,0 +1,29 @@
+
+// Program to print sum of first n even natural number
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i = 0,n  ,sum = 0;
+
+  printf("\t\t\tsum of first n even natural number \n\t\t\t**********************************\n");
+  printf("Enter value of n = ");
+  scanf("%d",&n);
+
+  while (i < n )
+  {
+    i++;
+    printf("%d + ",(2*i));
+   sum += (2*i); // expression to add even number to sum
+//   i++;
+  }
+  printf(" = %d \n",sum);
+  printf("\n\nsum = %d \n",sum);
+
+  // instruction to calculate sum of first n even natural number using formula  ;
+  printf("\n\n sum = %d \n",(n*(n+1)));
+  getch();
+  return 0;
+}
